6bbf1e3ee2884f06da0e9b4065c55a1d680b91ca
HoangKhiem3k <ledohoangkhiem3k@gmail.com>
2022-04-11 14:55:34 +0700

    setup route

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-
 import './App.css';
+import {createBrowserHistory} from 'history';
+import { Router } from 'react-router';
+import { HomeTemplate } from './template/HomeTemplate/HomeTemplate';
+import Home from './pages/Home/Home';
+import { Switch } from 'react-router-dom';
 
+export const history = createBrowserHistory();
 function App() {
   return (
-    <div className="App">
-      123
-    </div>
+    <Router history={history}>
+      <Switch>
+      <HomeTemplate path="/" exact Component={Home} />
+      </Switch>
+    </Router>
   );
 }
 
