76e1daae3e8909689b71e8be78fe7d15b536af03
HoangKhiem3k <ledohoangkhiem3k@gmail.com>
2022-04-11 22:24:11 +0700

    setup Slick(carousel film)

--- src/redux/configStore.js
+++ src/redux/configStore.js
@@ -1,9 +1,11 @@
 import { applyMiddleware, combineReducers, createStore } from "redux"
 import thunk from "redux-thunk"
 import { CarouselReducer } from "./reducers/CarouselReducer"
+import { QuanLyPhimReducer } from "./reducers/QuanLyPhimReducer"
 
 const rootReducer = combineReducers({
-    CarouselReducer
+    CarouselReducer,
+    QuanLyPhimReducer
 })
 
 export const store = createStore(rootReducer, applyMiddleware(thunk))
--- src/services/QuanLyPhimService.js
+++ src/services/QuanLyPhimService.js
@@ -9,6 +9,9 @@ export class QuanLyPhimService  extends baseService{
     layDanhSachBanner = () => {
         return this.get(`/api/QuanLyPhim/LayDanhSachBanner`);
     }
+    layDanhSachPhim = () => {
+        return this.get(`/api/QuanLyPhim/LayDanhSachPhim?maNhom=GP00`);
+    }
 }
 
 
