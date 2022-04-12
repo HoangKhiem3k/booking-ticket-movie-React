eeed6d968be4da5e77a139ec5b545f2f0d5a7c91
HoangKhiem3k <ledohoangkhiem3k@gmail.com>
2022-04-12 10:51:19 +0700

    render film dang chieu + sap chieu

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,23 +1,39 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import HomeMenu from './HomeMenu/HomeMenu'
+//Kết nối redux
 import { useSelector, useDispatch } from 'react-redux'
-import Film from '../../components/Film/Film'
 import MultipleRowSlick from '../../components/ReactSlick/MultipleRowSlick'
+import { layDanhSachPhimAction } from '../../redux/actions/QuanLyPhimActions';
+
 export default function Home(props) {
-    const { arrFilm } = useSelector(state => state.QuanLyPhimReducer)
+
+    const { arrFilm } = useSelector(state => state.QuanLyPhimReducer);
+    const dispatch = useDispatch();
+    console.log('propsHome', arrFilm);
+    // props.match.params
+
     // const renderFilms = () => {
-    //     return arrFilm.map((film, index) => {
-    //         return <Film key={index}/>
+    //     return arrFilm.map((phim, index) => {
+    //         return <Film key={index} />
+
 
     //     })
     // }
+
+    useEffect(()=>{
+        const action = layDanhSachPhimAction();
+        dispatch(action); //dispatch function từ thunk
+    },[])
+    
     return (
         <div>
-            <section className="text-gray-600 body-font" >
-                <div className="container px-5 py-24 mx-auto" >
-                    <MultipleRowSlick arrFilm={arrFilm}/>
+      
 
-                    {/* <div className="flex flex-wrap -m-4 " style={{ justifyContent: 'center' }}>
+            <section className="text-gray-600 body-font" >
+                <div className="container px-5 py-24 mx-auto " >
+
+                    <MultipleRowSlick arrFilm={arrFilm}/>
+                    {/* <div className="flex flex-wrap  " style={{ justifyContent: 'center' }}>
                         {renderFilms()}
                     </div> */}
                 </div>
