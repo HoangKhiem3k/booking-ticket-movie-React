df8c458953cfe3d135f401f2d3c62045a420c6db
HoangKhiem3k <ledohoangkhiem3k@gmail.com>
2022-04-13 14:41:55 +0700

    lazy loading for checkout template

--- src/App.js
+++ src/App.js
@@ -8,8 +8,11 @@ import News from './pages/News/News';
 import Login from './pages/Login/Login';
 import Register from './pages/Register/Register';
 import Detail from './pages/Detail/Detail';
+import CheckoutTemplate from './template/CheckoutTemplate/CheckoutTemplate';
+import Checkout from './pages/Checkout/Checkout';
+import {Suspense,lazy} from 'react';
 export const history = createBrowserHistory();
-
+const CheckoutTemplateLazy = lazy(() => import('./template/CheckoutTemplate/CheckoutTemplate'));
 function App() {
   return (
     <Router history={history}>
@@ -21,7 +24,11 @@ function App() {
         <HomeTemplate path="/detail/:id" exact Component={Detail} />
         <Route path="/login" exact Component={Login} />
         <Route path="/register" exact Component={Register} />
+        {/* <Suspense> gan loadding khi chua load xong */}
+        <Suspense fallback={<h1>loading ...</h1>}>
+          <CheckoutTemplateLazy path="/checkout/:id" exact Component={Checkout} />  
 
+        </Suspense>
       </Switch>
     </Router>
   );
